ca4af7ada72933993a5eb2f1b1fd6f66dba8bc3c
pavro_roman <roman.pavlov.1996@bk.ru>
2023-08-03 15:02:56 +0300

    Added many pages with seo and components

---
+++ src/api/podzadachi/controllers/podzadachi.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * podzadachi controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::podzadachi.podzadachi');
---
+++ src/api/podzadachi/routes/podzadachi.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * podzadachi router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::podzadachi.podzadachi');
